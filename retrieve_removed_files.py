d67140f6f3e00d31808b8d987e73d173130fa847
Eliran Shabat <shabat.eliran@gmail.com>
2021-03-06 17:22:43 +0200

    Add script to retrieve removed source files using the organization log

---
+++ retrieve_removed_files.py
@@ -0,0 +1,36 @@
+from argparse import ArgumentParser
+import re
+from tqdm import tqdm
+import os
+import shutil
+
+# image_organizer - INFO - Remove \'(.+)\' - duplicate of \'(.+)\'
+
+def _main():
+    log_file = args.log_file
+    
+    with open(log_file, 'rt') as f:
+        content = f.read()
+    
+    lines = content.splitlines()
+
+    pattern = r"image_organizer - INFO - Remove \'(.+)\' - duplicate of \'(.+)\'"
+
+    for curr_line in tqdm(lines, desc="Iterating over log lines"):
+        m = re.search(pattern, curr_line)
+        if m is not None:
+            dst_file = m.group(1)
+            src_file = m.group(2)
+            dst_dir = os.path.abspath(os.path.dirname(dst_file))
+            os.makedirs(dst_dir, exist_ok=True)
+            shutil.copyfile(src_file, dst_file)
+
+
+
+
+if __name__ == '__main__':
+    arg_parser = ArgumentParser(description="Retrive files that was removed during organization using the given log file")
+    arg_parser.add_argument("log_file", help="Organization's log file")
+    args = arg_parser.parse_args()
+
+    _main()
