6ecfc6e843c3630290e3fedec886fa0f93fe5072
Eliran Shabat <shabat.eliran@gmail.com>
2021-03-21 00:24:05 +0200

    First version with index - require more debugging

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ from datetime import datetime
 import imghdr
 import logging
 import time
+from tqdm import tqdm
+import re
 
 video_file_extensions = (".webm", ".mkv", ".flv", ".vob", ".ogv", ".ogg", ".drc", ".gif", ".gifv", ".mng", ".avi",
                          ".mts", ".m2ts", ".ts", ".mov", ".qt", ".wmv", ".yuv", ".rm", ".rmvb", ".asf", ".amv", ".mp4",
@@ -97,6 +99,11 @@ def create_logger(log_file, logger_name):
     return logger
 
 
+# TODO: verify that the logger exists
+def get_logger(logger_name):
+    return logging.getLogger(logger_name)
+
+
 def file_hash(file_path, chunk_size=2**24):
     """
     Compute the MD5 hash of the given file.
@@ -120,3 +127,105 @@ def delete_empty_dirs(root_dir, recursive):
             
             if len(os.listdir(path)) == 0:
                 os.rmdir(path)
+
+
+def list_media_files(repo_path, show_progress=False):
+    ls = list_subtree(repo_path, recursive=True)
+    ls_iterator = tqdm(ls, desc="Listing media files") if show_progress else ls
+    media_files = []
+    for f in ls_iterator:
+        try:
+            if is_media(f):
+                media_files.append(f)
+        except:
+            print(f"ERROR - failed to check if a file is a media file '{f}'")
+
+    return media_files
+
+
+def list_new_media_files(repo_path, last_update_time, show_progress=False):
+    all_media_files = list_media_files(repo_path, show_progress=show_progress)
+    new_media_files = []
+    all_media_iterator = tqdm(all_media_files, desc="Identifying new media") if show_progress else all_media_files
+    for f in all_media_iterator:
+        try:
+            mtime = datetime.fromtimestamp(os.stat(f).st_mtime)
+            ctime = datetime.fromtimestamp(os.stat(f).st_ctime)
+
+            if mtime > last_update_time or ctime > last_update_time:
+                new_media_files.append(f)
+
+        except:
+            print(f"ERROR - failed to get stats from '{f}'")
+
+    return new_media_files
+
+
+def list_modified_media_folders(repo_path, last_update_time, show_progress=False):
+    media_dirs = list_subtree_dirs(repo_path, recursive=False)
+    mod_dirs = []
+    dirs_iterator = tqdm(media_dirs, desc="Listing modified folders") if show_progress else media_dirs
+    for d in dirs_iterator:
+        try:
+            mtime = datetime.fromtimestamp(os.stat(d).st_mtime)
+            ctime = datetime.fromtimestamp(os.stat(d).st_ctime)
+            if mtime > last_update_time or ctime > last_update_time:
+                mod_dirs.append(d)
+        except:
+            print(f"ERROR - failed to get stats from '{d}'")
+
+    return mod_dirs
+
+
+def get_media_dirs(repo_dir):
+    all_dirs = list_subtree(repo_dir, recursive=False)
+    media_dirs = [dir_path for dir_path in all_dirs if
+                  re.search(r"\d{4}_\d{2}", os.path.basename(dir_path)) is not None]
+    return media_dirs
+
+
+def get_last_update_time(path):
+    mtime = datetime.fromtimestamp(os.stat(path).st_mtime)
+    ctime = datetime.fromtimestamp(os.stat(path).st_ctime)
+    if mtime > ctime:
+        return mtime
+    else:
+        return ctime
+
+
+def is_path_in_list(path, path_list):
+    norm_path = os.path.normpath(path)
+    for p in path_list:
+        if norm_path == os.path.normpath(p):
+            return True
+    return False
+
+
+def order_files(file1, file2):
+    """
+    Compute the order between two files, return the earlier file.
+    :param file1: The first file.
+    :param file2: The second file.
+    :return: 1 if the first file precede the second file,
+             2 if the second file precede the first file,
+             0 if the two files are actually the same file
+    """
+    mtime1 = datetime.fromtimestamp(os.stat(file1).st_mtime)
+    mtime2 = datetime.fromtimestamp(os.stat(file2).st_mtime)
+    ctime1 = datetime.fromtimestamp(os.stat(file1).st_ctime)
+    ctime2 = datetime.fromtimestamp(os.stat(file2).st_ctime)
+
+    if mtime1 < mtime2:
+        return 1
+    elif mtime1 > mtime2:
+        return 2
+    elif ctime1 < ctime2:
+        return 1
+    elif ctime1 > ctime2:
+        return 2
+    elif file1 < file2:
+        return 1
+    elif file1 > file2:
+        return 2
+    else:
+        return 0
